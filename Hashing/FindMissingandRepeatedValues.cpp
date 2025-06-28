ab43a2313bdefb3885f0f36d03ab764d5556b833
Sravankumarboini <sravankumarboini005@gmail.com>
2025-06-28 23:12:18 +0530

    modified problem

--- Hashing/FindMissingandRepeatedValues.cpp
+++ Hashing/FindMissingandRepeatedValues.cpp
@@ -1,1 +1,25 @@
 //https://leetcode.com/problems/find-missing-and-repeated-values/
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int a,b;
+        unordered_set<int> s;
+        vector<int> ans;
+        int actualsum=0,exsum=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                actualsum+=grid[i][j];
+                if(s.find(grid[i][j])!=s.end()){
+                    a=grid[i][j];
+                    ans.push_back(a);
+                }
+                s.insert(grid[i][j]);
+            }
+        }
+        exsum=(n*n)*(n*n+1)/2;
+        b=exsum+a-actualsum;
+        ans.push_back(b);
+        return ans;
+    }
+};
