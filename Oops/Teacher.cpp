75758ffc76a47d5dbc85f31eee8a323e352cb105
Sravankumarboini <sravankumarboini005@gmail.com>
2025-07-23 18:01:36 +0530

    ad oops file

---
+++ Oops/Teacher.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher{
+    //properties
+public:
+    string name;
+    string dept;
+    string subject;
+    double salary;
+    //methods
+    void chanegeDept(string newDept){
+        dept =newDept;
+    }
+};
+
+int main(){
+    Teacher t1;
+    t1.name="sravan";
+    t1.subject="dsa";
+    t1.dept="computer science";
+    t1.salary=25000;
+    cout<<t1.name;
+    return 0;
+}
