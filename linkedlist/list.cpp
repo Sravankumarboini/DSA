63c0e814cf9c5d1f1aa51eaa3836c365574d84de
Sravankumarboini <sravankumarboini005@gmail.com>
2025-07-26 21:59:19 +0530

    debug ll

--- linkedlist/list.cpp
+++ linkedlist/list.cpp
@@ -12,19 +12,32 @@ public:
 };
 class linkedlist{
     node* head;
+    node* tail;
 public:
     linkedlist(){
         head=NULL;
+        tail=NULL;
     }
     void push_front(int val){
         node* newNode=new node(val);
         if(head==NULL){
-            head=newNode;
+            head=tail=newNode;
             return;
         }
         else{
             newNode->next=head;
             head=newNode;
+        }
+    }
+    void push_back(int val){
+        node* newNode=new node(val);
+        if(head==NULL){
+            head=tail=newNode;
+            return;
+        }
+        else{
+            tail->next=newNode;
+            tail=newNode;
         }
     }
     void display(){
@@ -42,5 +55,7 @@ int main(){
     ll.push_front(20);
     ll.push_front(30);
     ll.push_front(40);
+    ll.display();
+    ll.push_back(50);
     ll.display();
 }
