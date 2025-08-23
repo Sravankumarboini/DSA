92c736ca208869151dd79665f39d35b9fae574bc
Sravankumarboini <sravankumarboini005@gmail.com>
2025-08-23 23:14:39 +0530

    add largestractangle in histogram

---
+++ Stack/longestRectangleInHiatogram.cpp
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/largest-rectangle-in-histogram/
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> s;
+        int n=heights.size();
+        vector<int> right(n,0);
+        vector<int> left(n,0);
+        //left smaller
+        for(int i=0;i<n;i++){
+            while(s.size()>0 && heights[s.top()]>=heights[i])
+            s.pop();
+            left[i]=s.empty()?-1:s.top();
+            s.push(i);
+        }
+        while(!s.empty()) s.pop();
+        //right smaller
+        for(int i=n-1;i>=0;i--){
+            while(s.size()>0 && heights[s.top()]>=heights[i])
+            s.pop();
+            right[i]=s.empty()?n:s.top();
+            s.push(i);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int width=right[i]-left[i]-1;
+            int currArea=width*heights[i];
+            ans=max(currArea,ans);
+        }
+        return ans;
+    }
+};
