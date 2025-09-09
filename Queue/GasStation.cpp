98eb9b890109707d131d7adeea3dcce3500d3ff1
Sravankumarboini <sravankumarboini005@gmail.com>
2025-09-09 21:55:11 +0530

    add gas station problem

---
+++ Queue/GasStation.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/gas-station/submissions/1765134248/
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalGas=0,totalCost=0;
+        for(int val:gas){
+            totalGas+=val;
+        }
+        for(int val:cost){
+            totalCost+=val;
+        }
+        if(totalGas<totalCost) return -1;
+        int start=0,curGas=0;
+        for(int i=0;i<gas.size();i++){
+            curGas+=(gas[i]-cost[i]);
+            if(curGas<0){
+                start=i+1;
+                curGas=0;
+            }
+        }
+        return start;
+    }
+};
